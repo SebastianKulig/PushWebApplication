f3099fe06804783e5adc9fd5b30e23e98ba68b3d
Kulig Sebastian <kuligsebastian8@gmail.com>
2021-12-07 17:30:57 +0100

    - add documentation

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.push_app.push"},{"l":"com.push_app.push.controller"},{"l":"com.push_app.push.firebase"},{"l":"com.push_app.push.model"},{"l":"com.push_app.push.service"},{"l":"com.push_app.push.utils"}]
